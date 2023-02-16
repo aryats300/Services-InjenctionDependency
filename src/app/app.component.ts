969963f2f2398a75cd438c82b0c56e9a0d168432
Arya TS <aryasreekala1997@gmail.com>
2023-02-16 17:11:59 +0530

    services and dependency injectable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // providers:[AccountsService]
 })
-export class AppComponent {
-  title = 'ServiceApp';
+export class AppComponent implements OnInit {
+ accounts:{name:string,status:string}[]=[];
+
+ constructor(private accountService : AccountsService){
+
+ }
+ ngOnInit(){
+  this.accounts=this.accountService.accounts;
+ }
 }
